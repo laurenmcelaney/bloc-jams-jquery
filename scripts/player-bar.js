8397b11d811e3c99a92de36ae240f991ebfd8694
lauren <mcelaneyl@gmail.com>
2019-04-02 22:37:43 -0700

    Updated player-bar

---
+++ scripts/player-bar.js
@@ -0,0 +1,28 @@
+{
+   $('button#play-pause').on('click', function() {
+     player.playPause();
+     $(this).attr('playState', player.playState);
+   });
+
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const nextSongIndex = currentSongIndex + 1;
+     const nextSong = album.songs[nextSongIndex];
+     if (nextSongIndex >= album.songs.length) { return; }
+
+     player.playPause(nextSong);
+  });
+
+   $('button#previous').on('click', function() {
+     if (player.playState !== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      const previousSong = album.songs[previousSongIndex];
+      if (previousSongIndex >= album.songs.length) { return; }
+
+     player.playPause(previousSong);
+  });
+}
