b52f241721a65dbd9c0835bfd1186c45ab99aa62
laurenmcelaney <45574516+laurenmcelaney@users.noreply.github.com>
2019-04-08 14:48:48 -0700

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -19,17 +19,19 @@
     player.skipTo(event.target.value);
   });
 
+  $('#volume-control input').on('input', function (event) {
+            player.setVolume(event.target.value);
+          });
+          
   setInterval ( () => {
-    if (player.playState !== 'playing'){
-      return;
-    }
+    if (player.playState !== 'playing'){ return; }
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime );
-      $('#time-control input').val(percent);
-    }, 1000);
-
+    $('#time-control input').val(percent);
+  }, 1000);
+}
    $('button#previous').on('click', function() {
      if (player.playState !== 'playing') { return; }
 
